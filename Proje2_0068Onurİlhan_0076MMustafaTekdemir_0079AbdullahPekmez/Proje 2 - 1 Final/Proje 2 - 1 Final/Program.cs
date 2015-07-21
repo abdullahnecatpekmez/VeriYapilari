4183edd82816855a9e9709cdbbf2c41fe3f28688
abdullahnecatpekmez <abdullah_pekmez89@hotmail.com>
2015-07-21 19:00:03 +0300

    VeriYapilari
    C#

---
+++ Proje2_0068Onurİlhan_0076MMustafaTekdemir_0079AbdullahPekmez/Proje 2 - 1 Final/Proje 2 - 1 Final/Program.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace calisma_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int secim = 0;
+            while (secim != 3)
+            {
+                secim = menu();
+                switch (secim)
+                {
+                    case 1:
+                        Console.Write("\n\tDairesel bağlı listeler icin n değerini giriniz ( atlama sayısı ):");
+                        n = Convert.ToInt32(Console.ReadLine());
+                        while (n < 1)
+                        {
+                            Console.Write("\n\tn değerini 0'dan büyük giriniz ( atlama sayısı ):");
+                            n = Convert.ToInt32(Console.ReadLine());
+                        }
+                        prg(n, secim);
+                        break;
+                    case 2:
+                        Console.Write("\n\tDairesel bağlı listeler için n değerini giriniz ( atlama sayısı ):");
+                        n = Convert.ToInt32(Console.ReadLine());
+                        while (n < 1)
+                        {
+                            Console.Write("\n\tn değerini 0'dan büyük giriniz ( atlama sayısı ):");
+                            n = Convert.ToInt32(Console.ReadLine());
+                        }
+
+                        DateTime ilk_zaman = new DateTime();
+                        ilk_zaman = DateTime.Now;
+
+                        DateTime son_zaman = new DateTime();
+                        son_zaman = DateTime.Now;
+
+                        TimeSpan fark = son_zaman - ilk_zaman;  // fark adında zaman farkını hesaplayan nesne olusturuluyor.                      
+                        int sayac = 0;
+                        while (fark.Seconds < 3)
+                        {
+                            prg(n, secim);
+                            son_zaman = DateTime.Now;
+                            fark = son_zaman - ilk_zaman;
+                            sayac++;
+                        }
+                        çizgi();
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.WriteLine("\tYapılan deney sonucunda bilgisayarınız 3 sn. de " + sayac + " kere çözmüştür...");
+                        Console.ResetColor();
+                        çizgi(); break;
+                }
+            }
+            Console.ReadKey();
+        }
+        public static void prg(int n, int secim)
+        {
+            int kat;
+            string str;
+            string[] arabalar = { "Beyaz", "Kirmizi", "Siyah", "Mavi", "Metalik", "Yesil", "Pembe", "Turuncu", "Turkuaz", "Eflatun" };
+            Random rnd = new Random();
+            ArrayList liste = new ArrayList(9);
+            Stack[] y = new Stack[3];
+            Queue[] k = new Queue[3];
+            Bliste[] l = new Bliste[3];
+            int ys = 0, ks = 0, ls = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    y[ys] = new Stack(10);
+                    for (int j = 0; j < 10; j++)
+                        y[ys].Push(arabalar[j]);
+                    liste.Add(y[ys]);
+                    ys++;
+                }
+                if (i % 3 == 1)
+                {
+                    k[ks] = new Queue(10);
+                    for (int j = 0; j < 10; j++)
+                        k[ks].Enqueue(arabalar[j]);
+                    liste.Add(k[ks]);
+                    ks++;
+                }
+                if (i % 3 == 2)
+                {
+                    l[ls] = new Bliste();
+                    for (int j = 0; j < 10; j++)
+                    {
+                        l[ls].ekleme(arabalar[j]);
+                        l[ls].n = n;
+                    }
+                    liste.Add(l[ls]);
+                    ls++;
+                }
+            }
+            int tur_say = 1;
+            while (k[0].Count != 0 || k[1].Count != 0 || k[2].Count != 0 || y[0].Count != 0 || y[1].Count != 0 || y[2].Count != 0 || !l[0].bosmu() || !l[1].bosmu() || !l[2].bosmu())
+            {
+                kat = rnd.Next(0, 9);
+                switch (kat % 3)
+                {
+                    case 0:
+                        if (k[kat / 3].Count != 0)
+                        {
+                            str = k[kat / 3].Dequeue().ToString();
+                            if (secim == 1)
+                            {
+                                Console.WriteLine(tur_say++ + ". Tur\nKat No:" + (kat + 1) + "\nCikan Arac :" + str);
+                                Console.WriteLine("Kalanlar");
+                                kuyruk_listele(k[kat / 3]);
+                                Console.WriteLine("\n");
+                            }
+                        } break;
+                    case 1:
+                        if (y[(kat - 1) / 3].Count != 0)
+                        {
+                            str = y[(kat - 1) / 3].Pop().ToString();
+                            if (secim == 1)
+                            {
+                                Console.WriteLine(tur_say++ + ". Tur\nKat No:" + (kat + 1) + "\nCikan Arac :" + str);
+                                Console.WriteLine("Kalanlar");
+                                yigin_listele(y[(kat - 1) / 3]);
+                                Console.WriteLine("\n");
+                            }
+                        } break;
+                    case 2:
+                        if (!l[(kat - 2) / 3].bosmu())
+                        {
+                            str = l[(kat - 2) / 3].çıkar();
+                            if (secim == 1)
+                            {
+                                Console.WriteLine(tur_say++ + ". Tur\nKat No:" + (kat + 1) + "\nCikan Arac :" + str);
+                                Console.WriteLine("Kalanlar");
+                                l[(kat - 2) / 3].listele();
+                                Console.WriteLine("\n");
+                            }
+                        } break;
+                }
+            }
+        }
+        public static void çizgi()
+        {
+            string str = "";
+            Console.Write(str.PadLeft(80, '-'));
+        }
+        public static int menu()
+        {
+            int sec;
+            Console.WriteLine("\n***********************************SEÇENEKLER***********************************");
+            Console.WriteLine("\t 1. Katlara yerleşen arabaları ve deney sonuçlarını gör... ");
+            Console.WriteLine("\t 2. Bilgisayarınızın 3 sn. içinde bu problemi çözme sayısını bul... ");
+            Console.WriteLine("\t 3. ÇIKIŞ...\n");
+            Console.WriteLine("***********************************SEÇENEKLER***********************************\n");
+            Console.Write("\tSeçiminizi giriniz: ");
+            sec = Convert.ToInt32(Console.ReadLine());
+            return sec;
+        }
+        public static void kuyruk_listele(Queue kuyruk)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Queue gecici = (Queue)kuyruk.Clone();
+
+            if (gecici.Count != 0)
+            {
+                while (gecici.Count > 0)
+                    Console.Write("--" + gecici.Dequeue().ToString());
+            }
+            else
+                Console.WriteLine("Bu katta hicbir arac bulunmamaktadir!");
+            Console.ResetColor(); // sıfırladık
+        }
+        public static void yigin_listele(Stack yigin)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Stack gecici = (Stack)yigin.Clone();
+
+            if (yigin.Count != 0)
+            {
+                while (gecici.Count > 0)
+                    Console.Write("--" + gecici.Pop().ToString());
+            }
+            else
+                Console.WriteLine("Bu katta hicbir arac bulunmamaktadir!");
+            Console.ResetColor(); // sıfırladık
+        }
+    }
+    class dugum
+    {
+        public object veri;
+        public dugum sonraki;
+        public dugum(object o)
+        {
+            veri = o;
+        }
+        public void yazdir() // Düğümün verisini yazdırır
+        {
+            if (veri != null)
+                Console.Write("--" + veri);
+        }
+    }
+    class Bliste
+    {
+        public dugum bas = new dugum(null);
+        public dugum son = new dugum(null);
+        public int n = -1;
+        public void ekleme(object o)
+        {
+            dugum y = new dugum(o);
+            if (bas.veri == null)
+            { y.sonraki = y; bas = y; }
+            else
+            {
+                dugum onceki = bas;
+                dugum gecici = bas.sonraki;
+                while (gecici.veri != bas.veri)
+                {
+                    onceki = gecici;
+                    gecici = gecici.sonraki;
+                }
+                onceki.sonraki = y;
+                y.sonraki = bas;
+                son = y;
+            }
+        }
+        public bool bosmu()
+        {
+            return bas.veri == null;
+        }
+        public string çıkar()
+        {
+            dugum önceki = bas;
+            dugum gecici = bas;
+            string str;
+            if (bas.sonraki == bas)
+            {
+                str = (string)bas.veri;
+                bas.veri = null;
+                return str;
+            }
+            else
+            {
+                if (n > 1)
+                    for (int i = 0; i < n - 1; i++)
+                    {
+                        önceki = gecici;
+                        gecici = gecici.sonraki;
+                    }
+                if (gecici.veri == bas.veri)
+                {
+                    bas = gecici.sonraki;
+                    son.sonraki = bas;
+                }
+                else
+                {
+                    önceki.sonraki = gecici.sonraki;
+                    son = önceki;
+                    bas = önceki.sonraki;
+                }
+                return (string)gecici.veri;
+            }
+        }
+        public void listele()
+        { // Bağlı Listeyi Baştan Sona Listeler
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            dugum etkin = bas;
+            etkin.yazdir(); etkin = etkin.sonraki;
+            while (etkin.veri != bas.veri)
+            { etkin.yazdir(); etkin = etkin.sonraki; }
+            if (bas.veri == null)
+                Console.WriteLine("Bu katta baska hicbir arac kalmamistir!");
+            Console.ResetColor(); // sıfırladık
+        }
+    }
+}
---
+++ Proje2_0068Onurİlhan_0076MMustafaTekdemir_0079AbdullahPekmez/Proje 2 - 2 Final/Proje 2 - 2 Final/Program.cs
@@ -0,0 +1,252 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace calisma_1
+{
+    class Kuyruk
+    {
+        private int kapasite;
+        private Musteri[] m;
+        private int ön;
+        private int arka;
+        private int kişi;
+        public Kuyruk(int s)
+        {
+            kapasite = s;
+            m = new Musteri[kapasite];
+            ön = 0;
+            arka = -1;
+            kişi = 0;
+        }
+        public void ekle(Musteri j)
+        {
+            if (arka == kapasite - 1)
+                arka = -1;
+            m[++arka] = j;
+            kişi++;
+        }
+        public Musteri sil()
+        {
+            Musteri temp = m[ön++];
+            if (ön == kapasite)
+                ön = 0;
+            kişi--;
+            return temp;
+        }
+        public int sayı()
+        {
+            return kişi;
+        }
+    }
+    class Musteri
+    {
+        public int no;
+        public int sure;
+    }
+    class OncelikKuyruk
+    {
+        public ArrayList liste;
+        public OncelikKuyruk()
+        {
+            liste = new ArrayList();
+        }
+        public void ekle(Musteri eklenen)  // ekle metodu
+        {
+            if (Bosmu())
+                liste.Add(eklenen);
+            else
+            {
+                for (int i = 0; i < liste.Count; i++)
+                {
+                    if (eklenen.sure > ((Musteri)liste[liste.Count - 1]).sure)
+                        liste.Add(eklenen);
+                    if (eklenen.sure < ((Musteri)liste[i]).sure)
+                    {
+                        liste.Insert(i, eklenen);
+                        break;
+                    }
+                }
+            }
+        }
+        public Musteri sil()  // sil metodu
+        {
+            Musteri m;
+            if (!Bosmu())
+            {
+                m = (Musteri)liste[0];
+                liste.RemoveAt(0);
+                return m;
+            }
+            else
+                return null;
+        }
+        public bool Bosmu()
+        {
+            return (liste.Count == 0);
+        }
+        public int eleman_say()
+        {
+            return liste.Count;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, secim, top_bek1, top_bek2;
+            Random rnd = new Random();
+
+            secim = 0;
+            while (secim != 3)
+            {
+                secim = menu();
+                switch (secim)
+                {
+                    case 1:
+                        Console.Write("\tLütfen N değerini giriniz:");
+                        N = Convert.ToInt32(Console.ReadLine());
+
+                        Kuyruk Q = new Kuyruk(N);
+                        OncelikKuyruk oncelik_k = new OncelikKuyruk();
+
+                        for (int i = 0; i < N; i++)
+                        {
+                            Musteri m1 = new Musteri();
+                            m1.no = i;
+                            m1.sure = rnd.Next(10, 600);
+                            Q.ekle(m1);
+                            oncelik_k.ekle(m1);
+                        }
+                        top_bek1 = fifo_bilgi_goster(Q, 1);
+                        top_bek2 = oncelik_bilgi_goster(oncelik_k, 1);
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("\n\nFIFO Kuyrugundaki ORTALAMA bekleme suresi : " + (top_bek1 / N) / 60 + " dk " + (top_bek1 / N) % 60 + " sn ");
+                        Console.WriteLine("\nONCELIK Kuyrugundaki ORTALAMA bekleme suresi : " + (top_bek2 / N) / 60 + " dk " + (top_bek2 / N) % 60 + " sn ");
+                        Console.WriteLine("\n Fark  Suresi: " + ((top_bek1 - top_bek2) / 60) + " dk " + ((top_bek1 - top_bek2) % 60) + " sn "); //genel süre farkı hesaplaniyor.
+                        Console.WriteLine("\n Fark Oranı: % " + (float)(100 * (top_bek1 - top_bek2)) / top_bek1); //fark oranı hesaplaniyor
+                        Console.ResetColor(); // sıfırladık
+                        break;
+
+                    case 2:
+                        Console.Write("\tLutfen N değerini giriniz:");
+                        N = Convert.ToInt32(Console.ReadLine());
+
+                        OncelikKuyruk oncelik_k1 = new OncelikKuyruk();
+                        OncelikKuyruk oncelik_k2 = new OncelikKuyruk();
+                        OncelikKuyruk oncelik_k3 = new OncelikKuyruk();
+
+                        Kuyruk Q1 = new Kuyruk(N);
+                        Kuyruk Q2 = new Kuyruk(N);
+                        Kuyruk Q3 = new Kuyruk(N);
+
+                        for (int i = 0; i < N; i++)
+                        {
+                            Musteri m = new Musteri();
+                            m.no = i;
+                            m.sure = rnd.Next(10, 600);
+                            if (oncelik_k1.eleman_say() < oncelik_k2.eleman_say() && oncelik_k1.eleman_say() < oncelik_k3.eleman_say())
+                            {
+                                oncelik_k1.ekle(m);
+                                Q1.ekle(m);
+                            }
+                            else if (oncelik_k2.eleman_say() < oncelik_k3.eleman_say())
+                            {
+                                oncelik_k2.ekle(m);
+                                Q2.ekle(m);
+                            }
+                            else
+                            {
+                                oncelik_k3.ekle(m);
+                                Q3.ekle(m);
+                            }
+                        }
+
+                        int[] top_islm_sure = new int[6];
+                        top_islm_sure[0] = fifo_bilgi_goster(Q1, 1);
+                        top_islm_sure[1] = fifo_bilgi_goster(Q2, 2);
+                        top_islm_sure[2] = fifo_bilgi_goster(Q3, 3);
+                        top_islm_sure[3] = oncelik_bilgi_goster(oncelik_k1, 1);
+                        top_islm_sure[4] = oncelik_bilgi_goster(oncelik_k2, 2);
+                        top_islm_sure[5] = oncelik_bilgi_goster(oncelik_k3, 3);
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("-----GENEL SONUCLAR-----");
+                        Console.WriteLine("--FIFO--   -- ONCELIK--   --FARK(sn)--  -- FARK ORANI( % )--");
+
+                        for (int i = 0; i < 3; i++)
+                        {
+                            Console.WriteLine("  " + top_islm_sure[i] + "\t\t " + top_islm_sure[i + 3] + " \t\t" + (top_islm_sure[i] - top_islm_sure[i + 3]) + "\t\t" + (float)(100 * ((top_islm_sure[i] - top_islm_sure[i + 3]))) / top_islm_sure[i]);
+                        }
+                        Console.ResetColor(); // sıfırladık
+                        break;
+
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        public static int oncelik_bilgi_goster(OncelikKuyruk k, int k_no)
+        {
+            Console.WriteLine("\n\n\n\t\t{0,2}. ÖNCELİK KUYRUKTAKİ gerçekleşen işlemler...", k_no);
+            çizgi();
+            int n = k.eleman_say(), t = 0, gen_top = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int s;
+                Musteri cikan = new Musteri();
+                cikan = k.sil();
+                s = cikan.sure;
+                t += s;
+                gen_top += t;
+                Console.WriteLine("{0,2}. Müşteri KUYRUKTA kendi işlemi dahil {1,4} sn. bekledi.(kendi işl. {2,4} sn.)", cikan.no + 1, t, s);
+                çizgi();
+            }
+            Console.WriteLine("\n\t{0,2} müşterinin toplam bekleme süresi: {1,4} dk. {2,2} sn. dir.", n, gen_top / 60, gen_top % 60);
+            Console.WriteLine("\n\tOrtalama bekleme süresi: {0,4} dk. {1,2} sn. dir.", (gen_top / n) / 60, (gen_top / n) % 60);
+            return gen_top;
+        }
+
+        public static int fifo_bilgi_goster(Kuyruk k, int k_no)
+        {
+            Console.WriteLine("\n\n\n\t\t\t{0,2}. KUYRUKTAKİ gerçekleşen işlemler...", k_no);
+            çizgi();
+            int N = k.sayı(), top1 = 0, gen_top = 0;
+            for (int i = 0; i < N; i++)
+            {
+                int s;
+                Musteri cikan1 = new Musteri();
+                cikan1 = k.sil();
+                s = cikan1.sure;
+                top1 += s;
+                gen_top += top1;
+                Console.WriteLine("{0,2}. Müşteri KUYRUKTA kendi işlemi dahil {1,4} sn. bekledi.(kendi işl. {2,4} sn.)", cikan1.no + 1, top1, s);
+                çizgi();
+            }
+            Console.WriteLine("\n\t{0,2} müşterinin toplam bekleme süresi: {1,4} dk. {2,2} sn. dir.", N, gen_top / 60, gen_top % 60);
+            Console.WriteLine("\n\tOrtalama bekleme süresi: {0,4} dk. {1,2} sn. dir.", (gen_top / N) / 60, (gen_top / N) % 60);
+            return gen_top;
+        }
+
+        public static int menu()
+        {
+            int sec;
+            Console.WriteLine("\n***********************************SEÇENEKLER***********************************");
+            Console.WriteLine("\t1. N kişi için FIFO ve Öncelik Kuyruğu arasındaki farkı gör.");
+            Console.WriteLine("\t2. N kişi için 3'er adet FIFO ve Öncelik Kuyruğu arasındaki farkı gör.");
+            Console.WriteLine("\t3. ÇIKIŞ...\n");
+            Console.WriteLine("***********************************SEÇENEKLER***********************************\n");
+            Console.Write("\tSeçiminizi giriniz:");
+            sec = Convert.ToInt32(Console.ReadLine());
+            return sec;
+        }
+        public static void çizgi()
+        {
+            string str = "";
+            Console.Write(str.PadLeft(80, '-'));
+        }
+    }
+}
